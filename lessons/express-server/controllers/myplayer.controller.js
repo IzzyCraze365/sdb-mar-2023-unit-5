a70d59a1516a5e5f1247808ee9510ddab0a2d76d
IzzyCraze365 <johnaisabella3@gmail.com>
2023-04-20 11:48:16 -0400

    End of Lectures Day 024

--- lessons/express-server/controllers/myplayer.controller.js
+++ lessons/express-server/controllers/myplayer.controller.js
@@ -1,9 +1,13 @@
+// Unit 5 - Day 021
+// player.controller.js
+// Rob did some crazy stuff in here on Day 24, so i just copied his code.
+
 const router = require("express").Router();
 // ! dbPath is the file path to the data based off of the relationship of the app.js file
 const dbPath = "./assets/player-db.json";
 // ! FS gives us access to the file system that is built-in with node
 const fs = require("fs");
-const { v4: uuidv4 } = require("uuid");
+const { v4: uuidv4 } = require("uuid"); //! Now it gives a unique ID
 
 // http://localhost:4000/player/add
 let playerArray = [];
@@ -15,7 +19,7 @@ router.post("/add", (req, res) => {
     // 1. Pull out the keys from the req.body
     const { firstName, lastName, position, team, jerseyNumber } = req.body;
     // 2. Create the object so it will inserted into the playerArray
-    const playerObect = {
+    const playerObject = {
       _id: uuidv4(),
       firstName: firstName.toUpperCase(),
       lastName: lastName.toUpperCase(),
@@ -25,7 +29,7 @@ router.post("/add", (req, res) => {
     };
     //  3. Add the players to the Array & write the completed array back to the file
 
-    playerArray.push(playerObect);
+    playerArray.push(playerObject);
 
     const isSaveComplete = save(playerArray);
 
@@ -50,77 +54,32 @@ router.get("/view-all", (req, res) => {
   }
 });
 
-// ! Delete one player
-// http://localhost:4000/player/delete/:id
-router.delete("/delete/:id", (req, res) => {
-  let playerArray = read();
+//! Delete one player
+// http:/localhost:4000/player/delete/:index
+// http:/localhost:4000/player/delete/:id
+router.delete("/delete/:id", (req, res) => { //! Changed "/:index" to "/:id"
+    let playerArray = read();
   try {
-    let id = req.params.id; //! changed to id
-
-    console.log(playerArray.length);
-    let index = playerArray.findIndex((player) => player._id == id);
-
-    playerArray = removeOne(index, playerArray); //! supplying the index to remove and the array to delete from. Changed to id
-    console.log(playerArray.length);
-    save(playerArray);
+    let id = req.params.id; //! Changed "/:index" to "/:id"
+/*     if (isNaN(index)) { //! since we are now using ID we will always fail
+      throw Error("Error: Index is Not a Number");
+    } */
+    //console.log(index); //! TEST
+    let index = playerArray.findIndex((player) => player._id == id); //! code that uses findIndex
+    playerArray = removeOne(+id, playerArray); // supplyiung the index to remove and the array to delete from.//! Changed "/:index" to "/:id"
+    // you need the plus to turn the index string into a number
+    //console.log(playerArray); //! TEST
+    save(playerArray); // This will update the Player Array in Postman so the changes persist
     res.json({ message: "player removed", player: playerArray });
   } catch (error) {
     res.json({ message: error.message });
   }
 });
 
-// ! Update a player
-// http://localhost:4000/player/update/?id=[uuid]
-router.patch("/update/", (req, res) => {
-  let playerArray = read();
-
-  try {
-    // 1. Pull out the keys from the req.body
-    const { firstName, lastName, position, team, jerseyNumber } = req.body;
-    // 2. Create the object so it will inserted into the playerArray
-    const playerObject = {
-      _id: req.query.id,
-      firstName: firstName.toUpperCase(),
-      lastName: lastName.toUpperCase(),
-      position: position,
-      team: team,
-      jerseyNumber: jerseyNumber,
-    };
-
-    // 3. assign the index value
-    let id = req.query.id;
-    let index = playerArray.findIndex((player) => player._id == id);
-
-    // 4. update the array
-    playerArray = updateOne(+index, playerObject, playerArray);
-    // 5. save the playerArray
-    save(playerArray);
-
-    res.json({ message: "player updated", player: playerArray });
-  } catch (error) {
-    res.json({ message: error.message });
-  }
-});
-
-function read() {
-  const file = fs.readFileSync(dbPath);
-  return JSON.parse(file);
-}
-
-function save(data) {
-  try {
-    fs.writeFileSync(dbPath, JSON.stringify(data));
-    return true;
-  } catch (error) {
-    console.log(error);
-    return false;
-  }
-}
-
-// function removeOne(index, myArray) {
-//   myArray.splice(index, 1);
-//   return myArray;
-// }
+/* function removeOne(index, myArray) { //! alternate Remove One code, for indexes
+  myArray.splice(index, 1);
+  return myArray;
+} */
 
 function removeOne(indexNumber, myArray) {
   let newArray = [];
@@ -131,6 +90,35 @@ function removeOne(indexNumber, myArray) {
   }
   return newArray;
 }
+
+//! Update a player
+// http:/localhost:4000/player/update/?index=1
+router.patch("/update/", (req, res) => {
+  let playerArray = read();
+  /*console.log(req.query);//! TEST
+  return res.send("update"); */
+  try {
+    // 1. Pull out the keys from the req.body
+    const { firstName, lastName, position, team, jerseyNumber } = req.body;
+    // 2. Create the object so it will inserted into the playerArray
+    const playerObject = {
+      firstName: firstName.toUpperCase(),
+      lastName: lastName.toUpperCase(),
+      position: position,
+      team: team,
+      jerseyNumber: jerseyNumber,
+    };
+    // 3. assign the index value
+    let index = req.query.index;
+    // 4. update the array
+    playerArray = updateOne(+index, playerObject, playerArray); // Our updateOne function, //! Remember the index is a string until we put a plus sign (+) to convert to a number
+    // 5. save the array
+    save(playerArray);
+    res.json({ mesage: "player updated", player: playerArray });
+  } catch (error) {
+    res.json({ message: error.message });
+  }
+});
 
 function updateOne(indexNumber, newObject, myArray) {
   // update only that index and return the entire array.
@@ -144,4 +132,20 @@ function updateOne(indexNumber, newObject, myArray) {
   }
   return newArray;
 }
+
+function read() {
+  const file = fs.readFileSync(dbPath);
+  return JSON.parse(file);
+}
+
+function save(data) {
+  fs.writeFileSync(dbPath, JSON.stringify(data), (error) => {
+    if (error) {
+      console.log(error);
+      return false;
+    }
+    return true;
+  });
+}
+
 module.exports = router;
